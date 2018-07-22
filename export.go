9d5ba62c874781d41d77c0a563c0c33ea0c29e72
ymohl-cl <ymohl-cl@student.42.fr>
2018-07-22 23:13:21 +0200

    update write csv and compress file

--- export.go
+++ export.go
@@ -5,7 +5,7 @@ import (
 	"compress/gzip"
 	"context"
 	"errors"
-	"io/ioutil"
+	"fmt"
 	"os"
 	"sync"
 	"time"
@@ -16,6 +16,11 @@ import (
 // Export client module
 type Export struct {
 	messageClient MessageRepository
+	csvFile       *os.File
+	compressFile  *os.File
+	zw            *gzip.Writer
+	mFile         sync.RWMutex
+	mCompress     sync.RWMutex
 }
 
 // MessageRepository define interface to give a messaging client
@@ -29,85 +34,115 @@ func (e *Export) SetMessageRepository(repo MessageRepository) {
 }
 
 // CSV exportation from a request
-func (e Export) CSV(ctx context.Context, request ExportRequest) (err error) {
+func (e *Export) CSV(ctx context.Context, request ExportRequest) (err error) {
 	var wg sync.WaitGroup
-	var mErr sync.RWMutex
-	var mMessage sync.RWMutex
-	var messages []Message
+	var m sync.RWMutex
 
 	if e.messageClient == nil {
 		return errors.New("message repository not define")
+	}
+	if err = e.initCSV(request.Filename); err != nil {
+		return err
+	}
+	defer e.closeCSV()
+	if request.Compression {
+		if err = e.initCompress(request.Filename); err != nil {
+			return err
+		}
+		defer e.closeCompress()
 	}
 
 	for _, device := range request.Devices {
 		wg.Add(1)
 		go func(target int64) {
 			defer wg.Done()
-
 			var errAsync error
 			var result []Message
 
+			m.Lock()
 			if result, errAsync = e.messageClient.List(ctx, target, request.From, request.To); err != nil {
-				mErr.Lock()
-				err = errors.New(err.Error() + errAsync.Error())
-				mErr.Unlock()
+				fmt.Printf("error: %s, to access message from bdd client to the device: %d\n", errAsync.Error(), target)
+				m.Unlock()
+				return
 			}
-			mMessage.Lock()
-			messages = append(messages, result...)
-			mMessage.Unlock()
+			m.Unlock()
+			if errAsync = e.writeMessage(result); err != nil {
+				fmt.Printf("error: %s, can't write message list to the device: %d\n", errAsync.Error(), target)
+				return
+			}
+			if request.Compression {
+				if errAsync = e.compressMessage(result); err != nil {
+					fmt.Printf("error compression messages: %s, to the device: %d\n", errAsync.Error(), target)
+					return
+				}
+			}
 		}(device)
 	}
 	wg.Wait()
-
-	if err = e.writeFile(request.Filename, messages); err != nil {
-		return err
-	}
-	if request.Compression {
-		if err = e.compressFile(request.Filename); err != nil {
-			return err
-		}
-	}
 	return nil
 }
 
-func (e Export) writeFile(filename string, messages []Message) error {
+func (e *Export) initCSV(filename string) error {
 	var err error
-	var file *os.File
 
-	if file, err = os.Create(filename); err != nil {
-		return err
-	}
-	defer file.Close()
-
-	if err = gocsv.MarshalFile(&messages, file); err != nil {
+	if e.csvFile, err = os.Create(filename); err != nil {
 		return err
 	}
 	return nil
 }
 
-func (e Export) compressFile(filename string) error {
+func (e *Export) initCompress(filename string) error {
 	var err error
-	var file, compressFile *os.File
-	var zw *gzip.Writer
-	var content []byte
 
-	if file, err = os.Open(filename); err != nil {
+	if e.compressFile, err = os.Create(filename + ".gz"); err != nil {
 		return err
 	}
-	defer file.Close()
-	if content, err = ioutil.ReadAll(file); err != nil {
-		return err
-	}
-	if compressFile, err = os.Create(filename + ".gz"); err != nil {
-		return err
-	}
-	defer compressFile.Close()
+	e.zw = gzip.NewWriter(e.compressFile)
+	return nil
+}
 
-	zw = gzip.NewWriter(compressFile)
-	if _, err := zw.Write(content); err != nil {
+func (e *Export) closeCSV() error {
+	var err error
+
+	if err = e.csvFile.Close(); err != nil {
 		return err
 	}
-	if err = zw.Close(); err != nil {
+	return nil
+}
+
+func (e *Export) closeCompress() error {
+	var err error
+
+	if err = e.zw.Close(); err != nil {
+		return err
+	}
+	if err = e.compressFile.Close(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (e *Export) writeMessage(messages []Message) error {
+	var err error
+
+	e.mFile.Lock()
+	defer e.mFile.Unlock()
+	if err = gocsv.MarshalFile(&messages, e.csvFile); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (e *Export) compressMessage(messages []Message) error {
+	var err error
+	var buf []byte
+
+	if buf, err = gocsv.MarshalBytes(&messages); err != nil {
+		return err
+	}
+	e.mCompress.Lock()
+	defer e.mCompress.Unlock()
+	if _, err := e.zw.Write(buf); err != nil {
 		return err
 	}
 	return nil
